7e1f89c3e9ae245e7329279394e72f5411bcbe0c
Li Yuanhe <yuanhe.li@vanderbilt.edu>
2025-02-02 01:55:43 -0600

    day4 finished

--- src/Day_4/D4.java
+++ src/Day_4/D4.java
@@ -5,9 +5,11 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class D4 {
-    private static int count = 0;
 
+    private static int count = 0;
+    private static int count2 = 0;
     private static final char[] truth = {'X', 'M', 'A', 'S'};
+    private static final int[][] dirs = {{1,0}, {1,1}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1}};
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/Day_4/input.txt");
         Scanner scanner = new Scanner(file);
@@ -31,7 +33,6 @@ public class D4 {
                         .toCharArray())
                 .toArray(char[][]::new);
 
-
         question1(arr);
         question2(arr);
     }
@@ -41,36 +42,47 @@ public class D4 {
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
                 if(board[i][j] == 'X'){
-                    boolean[][] visited = new boolean[m][n];
-                    dfs(board, i, j, 0, visited);
+                    for(int k = 0; k < 8; ++k){
+                        dfs(board, i, j, 0, k);
+                    }
                 }
             }
         }
         System.out.println("Q1: " + count);
     }
 
-    private static void dfs(char[][] board, int i, int j, int idx, boolean[][] visited){
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != truth[idx] || visited[i][j]){
+    private static void dfs(char[][] board, int i, int j, int idx, int k){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != truth[idx]){
             return;
         }
-
-        visited[i][j] = true;
 
         if(idx == 3) {
             count++;
             return;
         }
-        dfs(board, i+1, j, idx+1, visited);
-        dfs(board, i-1, j, idx+1, visited);
-        dfs(board, i, j+1, idx+1, visited);
-        dfs(board, i, j-1, idx+1, visited);
-        dfs(board, i+1, j+1, idx+1, visited);
-        dfs(board, i-1, j-1, idx+1, visited);
-        dfs(board, i-1, j+1, idx+1, visited);
-        dfs(board, i+1, j-1, idx+1, visited);
+        dfs(board, i+dirs[k][0], j+dirs[k][1], idx+1, k);
     }
 
     private static void question2(char[][] board){
+        int m = board.length, n = board[0].length;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(board[i][j] == 'A'){
+                    judge(board, i, j);
+                }
+            }
+        }
+        System.out.println("Q2: " + count2);
+    }
 
+    private static void judge(char[][] board, int i, int j){
+        if(i < 1 || j < 1 || i >= board.length-1 || j >= board[0].length-1){
+            return;
+        }
+        boolean condition1 = (board[i-1][j-1] == 'M' && board[i+1][j+1] == 'S') ||
+                (board[i-1][j-1] == 'S' && board[i+1][j+1] == 'M');
+        boolean condition2 = (board[i-1][j+1] == 'M' && board[i+1][j-1] == 'S') ||
+                (board[i-1][j+1] == 'S' && board[i+1][j-1] == 'M');
+        if(condition1 && condition2) count2++;
     }
 }
