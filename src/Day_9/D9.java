562eec233e3a3ba29af0123cd568c25be15e8382
Li Yuanhe <yuanhe.li@vanderbilt.edu>
2025-02-08 11:58:10 -0600

    day 9 setup

---
+++ src/Day_9/D9.java
@@ -0,0 +1,13 @@
+package Day_9;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class D9 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/Day_9/input.txt"));
+        String line = scanner.nextLine();
+
+    }
+}
